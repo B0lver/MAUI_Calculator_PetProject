eabd0c4155c659935d5b5998fac6ff82cae42f6f
Macnev <am_2222@mail.ru>
2023-02-01 21:08:26 +0300

    Основные операции

---
+++ Calculus_Library/Operations/BasicMath/Multiply.cs
@@ -0,0 +1,18 @@
+﻿using Calculus_Library.Interfaces;
+using Calculus_Library.Operations.Base;
+using System.Numerics;
+
+namespace Calculus_Library.Operations.BasicMath
+{
+    public class Multiply<T> : BinaryOperation<T> where T : INumber<T>
+    {
+        public Multiply(IValue<T> a, IValue<T> b) : base(a, b)
+        {
+        }
+
+        public override T Execute()
+        {
+            return a_value.GetValue() * b_value.GetValue();
+        }
+    }
+}
