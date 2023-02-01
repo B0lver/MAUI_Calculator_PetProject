549e7df60f0632de1e231cca91ecc26f0b8f6ea8
Macnev <am_2222@mail.ru>
2023-02-01 21:08:54 +0300

    Тестовое консольное приложение

---
+++ Calculation_TestConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Calculus_Library.Operations.Base;
+using Calculus_Library.Operations.BasicMath;
+
+Plus<int> operation1 = new(new BaseValue<int>(3), new BaseValue<int>(2));
+Minus<int> operation2 = new(operation1, new BaseValue<int>(1));
+var operation3 = new Multiply<int>(operation2, new BaseValue<int>(9));
+
+var res = operation3.GetValue();
+
+Console.WriteLine(res);
+Console.ReadLine();
