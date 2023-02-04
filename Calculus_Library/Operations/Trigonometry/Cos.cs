7706281c529b65d02636d81f146c6d5d04a0c92c
Macnev <am_2222@mail.ru>
2023-02-04 20:59:06 +0300

    операции тригонометрии

---
+++ Calculus_Library/Operations/Trigonometry/Cos.cs
@@ -0,0 +1,23 @@
+﻿using Calculus_Library.Interfaces;
+using Calculus_Library.Interfaces.Extensions;
+using Calculus_Library.Operations.Base;
+
+namespace Calculus_Library.Operations.Trigonometry
+{
+    public sealed class Cos : UnaryOperation
+    {
+        private readonly bool isDegreesUsed;
+        public Cos(IValue a, IUnaryExtension extension, bool isDegreesUsed = false) : base(a, extension)
+        {
+            this.isDegreesUsed = isDegreesUsed;
+        }
+
+        public override dynamic Execute()
+        {
+            if (isDegreesUsed) {
+                return Math.Cos((AValue.GetValue() / 180d) * Math.PI );
+            }
+            return Math.Cos(AValue.GetValue());
+        }
+    }
+}
---
+++ Calculus_Library/Operations/Trigonometry/Sin.cs
@@ -0,0 +1,24 @@
+﻿using Calculus_Library.Interfaces;
+using Calculus_Library.Interfaces.Extensions;
+using Calculus_Library.Operations.Base;
+
+namespace Calculus_Library.Operations.Trigonometry
+{
+    public sealed class Sin : UnaryOperation
+    {
+        private readonly bool isDegreesUsed;
+        public Sin(IValue a, IUnaryExtension extension = null, bool isDegreesUsed = false)
+            : base(a, extension)
+        {
+            this.isDegreesUsed = isDegreesUsed;
+        }
+
+        public override dynamic Execute()
+        {
+            if (isDegreesUsed){
+                return Math.Sin((AValue.GetValue() / 180d) * Math.PI);
+            }
+            return Math.Sin(AValue.GetValue()); ;
+        }
+    }
+}
