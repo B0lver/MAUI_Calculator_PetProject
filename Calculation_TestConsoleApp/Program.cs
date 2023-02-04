12229f7db6d89d2c6064b93c9376289c28af9318
Macnev <am_2222@mail.ru>
2023-02-04 20:59:28 +0300

    .

--- Calculation_TestConsoleApp/Program.cs
+++ Calculation_TestConsoleApp/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Calculus_Library.Operations.Base;
 using Calculus_Library.Operations.BasicMath;
 
-Plus<int> operation1 = new(new BaseValue<int>(3), new BaseValue<int>(2));
-Minus<int> operation2 = new(operation1, new BaseValue<int>(1));
-var operation3 = new Multiply<int>(operation2, new BaseValue<int>(9));
+Plus operation1 = new(new BaseValue(3), new BaseValue(2));
+Minus operation2 = new(operation1, new BaseValue(1));
+Multiply operation3 = new(operation2, new BaseValue(9));
+Divide operation4 = new(operation3, new BaseValue(35));
 
-var res = operation3.GetValue();
+var res = operation4.GetValue();
 
 Console.WriteLine(res);
 Console.ReadLine();
