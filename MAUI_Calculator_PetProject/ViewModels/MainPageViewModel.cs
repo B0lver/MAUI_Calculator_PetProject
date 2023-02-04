ec06eaf6f7dbead6b1eb019d7561246d858d5fbd
Macnev <am_2222@mail.ru>
2023-02-04 20:56:46 +0300

    Использование визуальных элементов

--- MAUI_Calculator_PetProject/ViewModels/MainPageViewModel.cs
+++ MAUI_Calculator_PetProject/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,10 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Calculus_Library.Interfaces;
+using Calculus_Library.Operations.Base;
+using Calculus_Library.Operations.BasicMath;
+using Calculus_Library.Operations.Trigonometry;
+using Calculus_Maui_Controls.Extensions.BinaryExtensions;
+using Calculus_Maui_Controls.Extensions.UnaryExtensions;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace MAUI_Calculator_PetProject.ViewModels
@@ -13,12 +19,34 @@ namespace MAUI_Calculator_PetProject.ViewModels
         [NotifyPropertyChangedFor(nameof(FullName))]
         string lastName;
 
+        [ObservableProperty]
+        BaseValue result;
+
+        [ObservableProperty]
+        IValue finalOperation;
         public string FullName => $"{FirstName} {LastName}";
 
         [RelayCommand]
         private void Submit()
         {
             Application.Current.MainPage.DisplayAlert("Полное имя", FullName, "Ок");
+        }
+
+        public string TestString { get; set; }
+        public MainPageViewModel()
+        {
+            FinalOperation = FormCalculation();
+            Result = new BaseValue(FinalOperation.GetValue());
+        }
+
+        private IValue FormCalculation()
+        {
+            var a = new BaseValue(1);
+            var b = new BaseValue(2);
+            var c = new Plus(a, b, new PlusExtension());
+            var d = new Sin(c, new SinExtension(), true);
+            var e = new Cos(c, new CosExtension(), true);
+            return new Plus(d, e, new PlusExtension());
         }
     }
 }
